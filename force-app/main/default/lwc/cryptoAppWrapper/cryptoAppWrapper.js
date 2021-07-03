20f103f5fd5a93d8a5bd8562d8bb37c0bcdeea39
Nikos <nik.ntf1@gmail.com>
2021-07-03 18:34:35 +0300

    Init api set up with Lwc

--- force-app/main/default/lwc/cryptoAppWrapper/cryptoAppWrapper.js
+++ force-app/main/default/lwc/cryptoAppWrapper/cryptoAppWrapper.js
@@ -1,3 +1,15 @@
 import { LightningElement } from 'lwc';
+import searchCrypto from "@salesforce/apex/CryptoCtrl.searchCrypto";
 
-export default class CryptoAppWrapper extends LightningElement {}
+export default class CryptoAppWrapper extends LightningElement {
+    searchTxt = '';
+    searchHandler(e){
+        console.log(e.detail);
+        console.log(e.detail.length);
+        if(e.detail.length >=3){
+            searchCrypto({userSelection:'assets',userSymbol:e.detail})
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
+        }
+    }
+}
