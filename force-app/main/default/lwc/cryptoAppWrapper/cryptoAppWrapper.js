c6224a0a14990ff5c3149ebfc9f38167912ace3b
Nikos <nik.ntf1@gmail.com>
2021-07-03 22:46:51 +0300

    Added cryptoData component

--- force-app/main/default/lwc/cryptoAppWrapper/cryptoAppWrapper.js
+++ force-app/main/default/lwc/cryptoAppWrapper/cryptoAppWrapper.js
@@ -3,12 +3,18 @@ import searchCrypto from "@salesforce/apex/CryptoCtrl.searchCrypto";
 
 export default class CryptoAppWrapper extends LightningElement {
     searchTxt = '';
+    cryptoData = [];
+    renderCryptoData = false;
     searchHandler(e){
         console.log(e.detail);
         console.log(e.detail.length);
         if(e.detail.length >=3){
             searchCrypto({userSelection:'assets',userSymbol:e.detail})
-            .then(res => console.log(res))
+            .then(res => {
+                console.log(res);
+                this.cryptoData = res.data;
+                this.cryptoData.length !=0?this.renderCryptoData = true:this.cryptoData = false;
+            })
             .catch(err => console.log(err));
         }
     }
